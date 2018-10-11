71d944e8945a2655a59e7ad71c4219e2b222e53a
kwoshvick <kwoshvick@gmail.com>
2018-10-11 10:27:28 +0300

    predict

--- twitter/twitter-crawler.py
+++ twitter/twitter-crawler.py
@@ -1,4 +1,4 @@
-
+import tweepy
 from twitter.credentials import Credentials
 
 
@@ -6,6 +6,12 @@ c = Credentials()
 
 api = c.authentinticate_twitter()
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print (tweet.text)
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print (tweet.text)
+
+
+for tweet in tweepy.Cursor(api.search,q="#nairobidiaries",count=100,
+                           lang="en",
+                           since="2016-04-03").items():
+    print (tweet.created_at, tweet.text)
