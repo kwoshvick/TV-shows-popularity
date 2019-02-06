0d78c2782533c0e718e511f7c2b2447f5f92bbb4
kwoshvick <kwoshvick@gmail.com>
2019-02-06 17:03:18 +0300

    csv test

--- twitter/twitter-crawler.py
+++ twitter/twitter-crawler.py
@@ -1,4 +1,5 @@
 import tweepy
+import csv
 from twitter.credentials import Credentials
 
 
@@ -11,7 +12,17 @@ api = c.authentinticate_twitter()
 #     print (tweet.text)
 
 
-for tweet in tweepy.Cursor(api.search,q="#nairobidiaries",count=100,
-                           lang="en",
-                           since="2016-04-03").items():
-    print (tweet.created_at, tweet.text)
+
+
+with open('../bahati.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["text"])
+    for tweet in tweepy.Cursor(api.search, q="#BeingBahati", count=1000,
+                               lang="en",
+                               since="2016-04-03").items():
+
+
+        writer.writerow([tweet.text,])
+# pass
+    # print (tweet.text)
+    # print("-----------")
