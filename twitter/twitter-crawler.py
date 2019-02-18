a2d82eafb9dbc5e9a5cbb703c861b6731d2456b7
kwoshvick <kwoshvick@gmail.com>
2019-02-18 15:50:14 +0300

    cleaner

--- twitter/twitter-crawler.py
+++ twitter/twitter-crawler.py
@@ -1,28 +1,83 @@
-import tweepy
+import os
 import csv
+import tweepy
+import datetime
 from twitter.credentials import Credentials
-
-
-c = Credentials()
-
-api = c.authentinticate_twitter()
-
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print (tweet.text)
+from twitter.cleaner import Cleaner
 
 
 
+credentials = Credentials()
+cleaner = Cleaner()
+api = credentials.authentinticate_twitter()
 
-with open('../bahati.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(["text"])
-    for tweet in tweepy.Cursor(api.search, q="#BeingBahati", count=1000,
-                               lang="en",
-                               since="2016-04-03").items():
+today = datetime.date.today()
+path = "../dataset/crawled/"+str(today)
+os.mkdir(path)
+
+hashtags =[
+    'BeingBahati'
+    'AuntieBoss',
+    'theTrend',
+    'HeyAmina',
+    'WickedEdition',
+    'Mafundi',
+    'PressPass',
+    'LivingWithEss',
+    'NTVWild',
+    'InHouse',
+    'LIT360',
+    'TopSport',
+    'NTVPropertyShow',
+    'ChurchillShow',
+    'ChurchillRaw',
+    'Crossover101',
+    'NTVJamrock',
+    'TeenRepublik',
+    'PasswordPlus',
+    'AMLiveNTV',
+    'PapaShirandula',
+    'TahidiHigh',
+    'KLive',
+    'OneLove',
+    'Machachari',
+    'setoEastAfrica',
+    'Bambika',
+    'MySweetCurse',
+    'WomanWithoutLimits',
+    'InspektaMwala',
+    'ShambaShapeUp',
+    '10over10',
+    'TheGirlNamedFeriha',
+    'FallIntoTemptation',
+    'Afrosinema',
+    'MorningExpress',
+    'TheEntrepreneur',
+    'MaishaMzuqa',
+    'TheSwitch',
+    'K24Alfajiri',
+    'TalkCentral',
+    'K24Tizi',
+    'NMGLeadershipForum',
+    'CaseFiles',
+    'TheChamwadaReport',
+    'tnlwithdrofweneke',
+    'Tukuza',
+    'NewViojaMahakamani',
+    'Vitimbi',
+    'ChiniYaMnazi',
+    'Daktari',
+    'AfricaLD',
+    'Tujaribu',
+    'DaringAbroad'
+]
 
 
-        writer.writerow([tweet.text,])
-# pass
-    # print (tweet.text)
-    # print("-----------")
+
+for hashtag in hashtags:
+    with open(path+'/' + hashtag + '.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["text", "label"])
+        for tweet in tweepy.Cursor(api.search, q="#"+hashtag,).items():
+            writer.writerow([cleaner.clean_tweets(tweet.text),])
+
